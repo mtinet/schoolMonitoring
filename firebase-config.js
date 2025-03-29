70828f3369045e9f27e7f53acc21ed781d41d80e
Jason Juhyun Kim <mtinet@hanmail.net>
2025-03-30 01:31:00 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDY0InT8eCZu5i7Bpa1m6r8lXzay4e6c0w",
+    authDomain: "vocatinalschool.firebaseapp.com",
+    databaseURL: "https://vocatinalschool-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "vocatinalschool",
+    storageBucket: "vocatinalschool.firebasestorage.app",
+    messagingSenderId: "131417384802",
+    appId: "1:131417384802:web:57dd705203970a45e26e3b",
+    measurementId: "G-J11KG7PFYV"
+  };
+
+  // Firebase 초기화
+  firebase.initializeApp(firebaseConfig);
